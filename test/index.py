439c095e58cc0db89cdd356e452a4da6dd7e4eaf
TKDKid1000 <rhoneeiler@gmail.com>
2021-12-06 20:17:10 -0800

    fix cli building issue

--- test/index.py
+++
@@ -1,8 +0,0 @@
-import discord
-
-class UserClient(discord.Client):
-        async def on_ready(self):
-                print("logged in as ", self.user)
-
-client = UserClient()
-client.run("FqlUybjC619jO3Mc5ul7tA9whImEQrmwLiZYiTAp4gAxLoiEVOJE9uCQurB2qFkCwdAbWhW3QvKK4TRf8eZ3")
